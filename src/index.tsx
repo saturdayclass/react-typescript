ac9479acd1d504987cc2eeb84203e3c189f154a5
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-17 17:14:28 +0700

    update: added example ref with ts'

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
-import SearchUser from './classes/SearchUser';
+import SearchUser from './refs/SearchUser';
 
 const App = () => {
-  const listUser = [{ name: 'Raihan', age: 20 }];
+  // const listUser = [{ name: 'Raihan', age: 20 }];
   return (
     <div>
-      <SearchUser users={listUser} />
+      <SearchUser />
     </div>
   );
 };
