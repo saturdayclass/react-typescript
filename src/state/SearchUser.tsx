c010128cb7e336232dee8d614a41f2832de519df
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-17 15:04:52 +0700

    update: added result of another example

--- src/state/SearchUser.tsx
+++ src/state/SearchUser.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
 
-const userList = [
-  { name: 'Sarah', age: 27 },
-  { name: 'Jhon', age: 20 },
-  { name: 'Raihan', age: 23 },
-];
-
 const SearchUser: React.FC = () => {
-  const [name, setName] = useState('');
+  const users = [
+    { name: 'Sarah', age: 27 },
+    { name: 'Jhon', age: 20 },
+    { name: 'Raihan', age: 23 },
+  ];
 
-  const onClick = () => {};
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((data) => {
+      return data.name === name;
+    });
+
+    console.log(foundUser);
+
+    setUser(foundUser);
+  };
 
   return (
     <div>
@@ -20,6 +29,10 @@ const SearchUser: React.FC = () => {
         onChange={(e) => setName(e.target.value)}
       />
       <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
     </div>
   );
 };
